87ca368dadfa9e7cb2e2bcc1d18d8782a9886a28
Aditya Hilman <aditya.hilman@primaku.com>
2026-01-16 14:15:41 +0700

    Refactor noscript tag for Google Tag Manager integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,14 +43,19 @@ export default function RootLayout({
       <body
         className={`${josefinSans.variable} font-sans antialiased`}
       >
-        <noscript>
+        {/* <noscript>
           <iframe
             src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
-        </noscript>
+        </noscript> */}
+        <noscript 
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
         {children}
         <Script
           src={snapUrl}
