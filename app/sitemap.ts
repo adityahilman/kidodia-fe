1be4ec030a549a06dadb2ad072da5404da974fb1
Aditya Hilman <aditya.hilman@primaku.com>
2026-02-17 07:34:18 +0700

    Refactor getProducts function to include error handling and improve fetch logic for sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,22 @@
 import { MetadataRoute } from "next";
 
 async function getProducts() {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`, {
-        next: {
-            revalidate: 60
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products/`, {
+            next: {
+                revalidate: 60
+            }
+        });
+        if (!res.ok) {
+            throw new Error('Failed to fetch products: ' + res.statusText);
         }
-    });
-    return res.json();
+        return await res.json();
+
+    } catch (error) {
+        console.error('Error fetching products for sitemap:', error);
+        return [];
+    }
+    
 }
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
