d59adf66b399e5541154eef2bf89a21860ecdf40
Aditya Hilman <aditya.hilman@primaku.com>
2026-02-28 08:59:19 +0700

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,43 +1,23 @@
 import { MetadataRoute } from "next";
+import { products } from "@/lib/product";
 
-async function getProducts() {
-    try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/`, {
-            next: {
-                revalidate: 60
-            }
-        });
-        if (!res.ok) {
-            throw new Error('Failed to fetch products: ' + res.statusText);
-        }
-        return await res.json();
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
-    } catch (error) {
-        console.error('Error fetching products for sitemap:', error);
-        return [];
-    }
-    
-}
+  const productUrls = products.map((product: any) => ({
+    url: `${baseUrl}/product/${product.slug}`,
+    lastModified: new Date().toISOString(),
+  }));
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-
-    const products = await getProducts();
-    const productUrls = products.map((product: any) => ({
-        url: `${process.env.NEXT_PUBLIC_BASE_URL}/products/${product.slug}`,
-        lastModified: new Date().toISOString()
-    }));
-
-    return [
-        {
-            url: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
-            lastModified: new Date().toISOString()
-        },
-        {
-            url: `${process.env.NEXT_PUBLIC_BASE_URL}/products`,
-            lastModified: new Date().toISOString()
-        },
-        ...productUrls
-        
-    ]
-
+  return [
+    {
+      url: `${baseUrl}/`,
+      lastModified: new Date().toISOString(),
+    },
+    {
+      url: `${baseUrl}/products`,
+      lastModified: new Date().toISOString(),
+    },
+    ...productUrls,
+  ];
 }
